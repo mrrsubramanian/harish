df86b2c869d7a602d1a908016b4e206c72116305
mrrsubramanian <harish30497@gmail.com>
2017-06-15 10:09:20 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("the given no is even\n");
+	}
+	else
+	{
+		printf("the given no is odd\n");
+	}
+    return 0;
+}   
